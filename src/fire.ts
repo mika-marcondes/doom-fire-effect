8cdc8ed860cb6bfbe53c03f55eef05b42a7eb9b8
Mika <mika.marcondes2@gmail.com>
2022-08-17 16:39:22 -0300

    :art: Added color palette, resize canvas

--- src/fire.ts
+++ src/fire.ts
@@ -2,12 +2,14 @@ interface Fire {
   width: number
   height: number
   pixelArray: Array<number>
+  colorPalette: Array<any>
 }
 
 const fire: Fire = {
-  width: 10,
-  height: 10,
+  width: 50,
+  height: 50,
   pixelArray: [],
+  colorPalette: [{"r":7,"g":7,"b":7},{"r":31,"g":7,"b":7},{"r":47,"g":15,"b":7},{"r":71,"g":15,"b":7},{"r":87,"g":23,"b":7},{"r":103,"g":31,"b":7},{"r":119,"g":31,"b":7},{"r":143,"g":39,"b":7},{"r":159,"g":47,"b":7},{"r":175,"g":63,"b":7},{"r":191,"g":71,"b":7},{"r":199,"g":71,"b":7},{"r":223,"g":79,"b":7},{"r":223,"g":87,"b":7},{"r":223,"g":87,"b":7},{"r":215,"g":95,"b":7},{"r":215,"g":95,"b":7},{"r":215,"g":103,"b":15},{"r":207,"g":111,"b":15},{"r":207,"g":119,"b":15},{"r":207,"g":127,"b":15},{"r":207,"g":135,"b":23},{"r":199,"g":135,"b":23},{"r":199,"g":143,"b":23},{"r":199,"g":151,"b":31},{"r":191,"g":159,"b":31},{"r":191,"g":159,"b":31},{"r":191,"g":167,"b":39},{"r":191,"g":167,"b":39},{"r":191,"g":175,"b":47},{"r":183,"g":175,"b":47},{"r":183,"g":183,"b":47},{"r":183,"g":183,"b":55},{"r":207,"g":207,"b":111},{"r":223,"g":223,"b":159},{"r":239,"g":239,"b":199},{"r":255,"g":255,"b":255}]
 }
 
 function start() {
@@ -15,7 +17,7 @@ function start() {
   createFireSource(fire)
   renderFire(fire)
   
-  setInterval(calculateFirePropagation, 1000, fire)
+  setInterval(calculateFirePropagation, 50, fire)
 }
 
 function createFireDataStructure(fire: Fire) {
@@ -52,6 +54,7 @@ function updateFireIntensityPerPixel(currentPixelIndex: number, fire: Fire) {
 }
 
 function renderFire(fire: Fire) {
+  const debug: boolean = false
   let html = "<table>"
   
   for (let row = 0; row < fire.height; row++) {
@@ -61,17 +64,23 @@ function renderFire(fire: Fire) {
       const pixelIndex = column + fire.width * row
       const fireIntensity = fire.pixelArray[pixelIndex]
       
-      html += "<td>"
-      html += `<div class="pixel-index">${pixelIndex}</div>`
-      html += fireIntensity
-      html += "</td>"
+      if (debug) {
+        html += "<td>"
+        html += `<div class="pixel-index">${pixelIndex}</div>`
+        html += fireIntensity
+        html += "</td>"
+      } else {
+        const color = fire.colorPalette[fireIntensity]
+        const colorString: string = `${color.r},${color.g},${color.b}`
+        html += `<td class="pixel" style="background-color: rgb(${colorString})">`
+        html += '</td>'
+      }
     }
     
     html += "</tr>"
   }
   
   html += "</table>"
-  
   document.querySelector("#fireCanvas")!.innerHTML = html
 }
 
